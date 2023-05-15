1eeea10dcb1466055e20d796b59a71cd23d49ce6
Zach Cohn <zcohn@wustl.edu>
2023-05-14 20:53:45 -0500

    fixed async problems in update.js

--- src/components/SearchableSeriesList/SearchableSeriesList.js
+++ src/components/SearchableSeriesList/SearchableSeriesList.js
@@ -1,57 +1,72 @@
-import React from "react";
-import { useState, useEffect } from "react";
-// import styles
+import React, { useState, useEffect } from "react";
 import styles from "./SearchableSeriesList.module.css";
 import CustomToggle from "../CustomToggle/CustomToggle";
 import { CircularProgress } from "@mui/material";
 import Series from "../SeriesListing/Series/Series";
 
 function SearchableSeriesList(props) {
+  const [sortedSeriesInfo, setSortedSeriesInfo] = useState([]);
+
+  useEffect(() => {
+    // Sort the seriesInfo whenever it changes
+    sortSeriesInfo();
+  }, [props.seriesInfo]);
+
+  const sortSeriesInfo = () => {
+    // Copy the seriesInfo array
+    let sortedInfo = [...props.seriesInfo];
+
+    // Perform sorting based on your desired criteria
+    sortedInfo.sort((a, b) => {
+      // Replace the condition with your custom sorting logic
+      // Here, sorting based on the series name in ascending order
+      if (a.series < b.series) return -1;
+      if (a.series > b.series) return 1;
+      return 0;
+    });
+
+    setSortedSeriesInfo(sortedInfo);
+  };
 
   return (
     <div className={styles["series-list"]}>
-      {/* Use a ternary operator to conditionally render the loading spinner */}
       {props.loading ? (
         <div className="w-100 pt-4 h-100 d-flex justify-content-center align-items-center">
           <CircularProgress style={{ color: "black" }} />
         </div>
       ) : (
         <>
-          {/* Wrap the collapse-all-toggle div and the seriesInfo.map inside a fragment */}
           <div className={styles["collapse-all-toggle-container"]}>
-              <p>Collapse Series</p>
-              <CustomToggle
-                checked={props.allCollapsed}
-                onChange={props.toggleCollapseAll}
-                label="Collapse All"
-              />
+            <p>Collapse Series</p>
+            <CustomToggle
+              checked={props.allCollapsed}
+              onChange={props.toggleCollapseAll}
+              label="Collapse All"
+            />
           </div>
 
-          {props.seriesInfo == [] ? (
+          {sortedSeriesInfo.length === 0 ? (
             <div className="w-100 d-flex justify-content-center">
               <p>No results found</p>
             </div>
           ) : (
-            props.seriesInfo.map((series) => {
-              // if the seriesInfo array is empty, render a message
-
-              // Use a variable to store the Series component if it should be rendered
+            sortedSeriesInfo.map((series) => {
               let component = null;
               if (
                 props.seriesList.includes(series.series) ||
-                props.seriesList.length == 0
+                props.seriesList.length === 0
               ) {
-                // ensure that there exists at least one parent with more than 0 children
                 let nonEmptyParent = false;
                 if (series.parentWorks.length > 0) {
                   for (let i = 0; i < series.parentWorks.length; i++) {
                     if (series.parentWorks[i].children.length > 0) {
                       nonEmptyParent = true;
+                      break;
                     }
                   }
                 }
 
-                if (nonEmptyParent == true) {
+                if (nonEmptyParent) {
                   component = (
                     <Series
                       key={series.seriesID}
