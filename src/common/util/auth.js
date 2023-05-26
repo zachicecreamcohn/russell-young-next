7ae8140e591b44ce04d68b7a04d6744f43e186ac
Zach Cohn <zcohn@wustl.edu>
2023-05-26 14:55:19 -0400

    progress on account details page

--- src/common/util/auth.js
+++ src/common/util/auth.js
@@ -16,3 +16,40 @@ export function checkLogin() {
         }
       });
     }
+
+  
+
+  
+export class User {
+  constructor() {
+    this.username = null;
+    this.email = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.company = null;
+    
+
+  } 
+
+  async isLoggedIn() {
+   // use session to check if the user is logged in
+    const response = await fetch("/api/userManagement/checkLogin");
+    const data = await response.json();
+    return data.loggedIn;
+  }
+
+  async fetchUser() {
+    if (await this.isLoggedIn()) {
+      const response = await fetch("/api/userManagement/loggedInUser");
+      const data = await response.json();
+      if (data.success) {
+        this.username = data.user.username;
+        this.email = data.user.email;
+        this.firstName = data.user.first_name;
+        this.lastName = data.user.last_name;
+        this.company = data.user.company;
+      }
+    }
+  }
+}
+
---
+++ src/components/Settings/MyAccount/UserDetails/UserDetails.js
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import styles from "./UserDetails.module.css";
+import { TextField, Button } from "@mui/material";
+
+function UserDetails(props) {
+  const [userDetailsData, setUserDetailsData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    company: "",
+  });
+
+  const handleChange = (e) => {
+    setUserDetailsData({
+      ...userDetailsData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userDetailsData);
+    // Here you would typically pass the userDetails to a function that handles your form submission
+  };
+
+  return (
+      <><TextField
+
+      label="First Name"
+      variant="outlined"
+      size="small"
+      sx={{
+        width: 250,
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: "black",
+          },
+          "&:hover fieldset": {
+            borderColor: "black",
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: "black",
+          },
+        },
+        marginBottom: 2,
+        marginTop: 5
+      }} /><TextField
+
+        label="Last Name"
+        variant="outlined"
+        size="small"
+        sx={{
+          width: 250,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "black",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "black",
+            },
+          },
+          marginBottom: 2,
+        }} /><TextField
+
+        label="Email"
+        variant="outlined"
+        size="small"
+        sx={{
+          width: 250,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "black",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "black",
+            },
+          },
+          marginBottom: 2,
+        }} /><TextField
+
+        label="Username"
+        variant="outlined"
+        size="small"
+        sx={{
+          width: 250,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "black",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "black",
+            },
+          },
+          marginBottom: 2,
+        }} /><TextField
+
+        variant="outlined"
+        value="**********"
+        disabled
+        size="small"
+        sx={{
+          width: 250,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "black",
+            },
+            "&.Mui-focused: fieldset": {
+              borderColor: "black",
+            },
+           
+          },
+          marginBottom: 2,
+        }} /><TextField
+
+        label="Company"
+        variant="outlined"
+        size="small"
+        sx={{
+          width: 250,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "black",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "black",
+            },
+          },
+        }} /></>
+  );
+}
+      
+export default UserDetails;
