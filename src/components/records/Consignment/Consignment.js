4e130b9199223de81f6267453d0051a5eefbe0b9
Zach Cohn <zcohn@wustl.edu>
2023-05-28 19:29:59 -0400

    worked on consignment functions

--- src/components/records/Consignment/Consignment.js
+++ src/components/records/Consignment/Consignment.js
@@ -4,30 +4,599 @@ import styles from "./Consignment.module.css";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-
+import Datepicker from "@/components/_common/grid/Datepicker/Datepicker";
 import { useEffect, useState } from "react";
-
+import Consign from "../Consign/Consign";
 import SkeletonTable from "../SkeletonTable/SkeletonTable";
 
 function Consignment(props) {
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-    const [dataLoaded, setDataLoaded] = useState(false);
 
-    useEffect(() => {
-        console.log("Consignment component mounted");
-    }, []);
 
-    return (
-        <div className={styles.container}>
-            {
-                dataLoaded ? (
-                    <p>Consignment</p>
-                ) : (
-                    <SkeletonTable />
-                )
-            }
-        </div>
-    );
+
+  useEffect(() => {
+    // after 3 seconds, set dataLoaded to true
+    setTimeout(() => {
+      setDataLoaded(true);
+    }, 500);
+
+
+    props.setRightContent(<Consign
+    alertSuccess={props.alertSuccess}
+    alertError={props.alertError}
+    />)
+  }, []);
+
+  function handleChange(params) {
+    console.log(params);
+    props.alertSuccess("Saved!");
+    
+  }
+
+  function handleCellEditingStopped(params) {
+    const updatedRowData = rowData.map((row) => {
+      if (row.id === params.data.id) {
+        return { ...row, [params.colDef.field]: params.value };
+      }
+      return row;
+    });
+    handleChange(params);
+  }
+  
+    const rowData = [  
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },{
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    },
+    {
+        date: "2021-01-01",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 1",
+        artwork: "Artwork 1",
+        colorway: "Colorway 1",
+        cat: "Cat 1",
+        note: "Note 1",
+        price: "$100",
+    },
+    {
+        date: "2021-01-02",
+        from: "John Doe",
+        to: "Jane Doe",
+        series: "Series 2",
+        artwork: "Artwork 2",
+        colorway: "Colorway 2",
+        cat: "Cat 2",
+        note: "Note 2",
+        price: "$200",
+    }
+    ];
+
+  const columnDefs = [
+    {
+      headerName: "Date",
+      field: "date",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      cellRendererFramework: (params) => <Datepicker value={params.value} onChange={(newValue) => params.setValue(newValue)} />,
+      cellStyle: { display: "flex",
+    cursor: "pointer" },
+    },
+    {
+      headerName: "From",
+      field: "from",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "To",
+      field: "to",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Series",
+      field: "series",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Artwork",
+      field: "artwork",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Colorway",
+      field: "colorway",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Cat #",
+      field: "cat",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Note",
+      field: "note",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      }
+    },
+    {
+      headerName: "Price",
+      field: "price",
+      sortable: true,
+      filter: true,
+    //   resizable: true,
+      editable: true,
+      cellStyle: {
+        cursor: "pointer"
+      },
+    },
+  ];
+
+  useEffect(() => {
+    console.log("Consignment component mounted");
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {dataLoaded ? (
+        <>
+          <div className={`ag-theme-alpine custom-ag-grid ${styles["ag-theme-alpine"]}`}>
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}
+            domLayout="autoHeight"
+            onGridSizeChanged={(params) => {
+              params.api.sizeColumnsToFit();
+            }}
+            onCellValueChanged={(params) => {
+              handleChange(params);
+            }}
+            onCellEditingStopped={(params) => {
+                handleCellEditingStopped(params);
+              }}
+            
+          />
+            </div>
+        </>
+      ) : (
+        <SkeletonTable />
+      )}
+
+    </div>
+  );
 }
 
 export default Consignment;
--- src/pages/records/index.js
+++ src/pages/records/index.js
@@ -1,16 +1,16 @@
-import Body from "@/components/_common/Body/Body"
-import { checkLogin } from "@/common/util/auth"
-import { useEffect, useState } from "react"
-import { CircularProgress } from "@mui/material";
+import Body from "@/components/_common/Body/Body";
+import { checkLogin } from "@/common/util/auth";
+import { useEffect, useState } from "react";
+import { CircularProgress, useMediaQuery } from "@mui/material";
 import Menu from "@/components/_common/Menu/Menu";
 import Consignment from "@/components/records/Consignment/Consignment";
 import Inventory from "@/components/records/Inventory/Inventory";
 import styles from "./Records.module.css";
-
+import toast, { Toaster } from "react-hot-toast";
 function Records() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [activeMenuItem, setActiveMenuItem] = useState("CONSIGNMENT");
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState("CONSIGNMENT");
+  const [menuRightContent, setMenuRightContent] = useState(null);
 
   useEffect(() => {
     checkLogin()
@@ -26,44 +26,71 @@ function Records() {
       });
   }, []);
 
+  function alertError(message) {
+    toast.error(message, {
+      style: {
+        boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.3)",
+      },
+      position: "top-center",
+    });
+  }
+
+  function alertSuccess(message) {
+    toast.success(message, {
+      style: {
+        boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.3)",
+      },
+      position: "top-center",
+    });
+  }
+
+
+
+
+
   if (!isLoggedIn) {
     return (
       <Body center>
-<CircularProgress
+        <CircularProgress
           sx={{
             color: "#000000",
           }}
-        />      </Body>
+        />{" "}
+      </Body>
     );
   }
 
-    return (
-        <Body
-    center
-    direction="column"
-    Tabs={true}
-    activeTab="records"
-    >
 
+  return (
+    <Body center direction="column" Tabs={true} activeTab="records">
+      <Menu
+            menuItems={["CONSIGNMENT", "INVENTORY"]}
+            activeMenuItem={activeMenuItem}
+            setActiveMenuItem={setActiveMenuItem}
+            showUserFunctions={false}
+            sticky={true}
+            wide={true}
+            rightContent={
+              <div className={styles.rightContent}>
+                {menuRightContent}
+              </div>
+            }
 
+          />
       <div className={styles.container}>
-        <div className={styles.left}>
-<Menu
-        title="Records"
-        menuItems={["CONSIGNMENT", "INVENTORY"]}
-        activeMenuItem={activeMenuItem}
-        setActiveMenuItem={setActiveMenuItem}
-        showUserFunctions={false}
-      />
-      </div>
-      <div className={styles.right}>
-        {activeMenuItem === "CONSIGNMENT" && <Consignment />}
-        {activeMenuItem === "INVENTORY" && <Inventory />}
-        </div>
-      </div>
+        
+          
 
+          {activeMenuItem === "CONSIGNMENT" && (
+            <Consignment alertError={alertError} alertSuccess={alertSuccess} setRightContent = {setMenuRightContent} />
+          )}
+          {activeMenuItem === "INVENTORY" && (
+            <Inventory alertError={alertError} alertSuccess={alertSuccess} />
+          )}
+        </div>
+      <Toaster containerStyle={{ top: "50px" }} />
     </Body>
-    )
+  );
 }
 
-export default Records
+export default Records;
