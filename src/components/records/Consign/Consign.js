0492f343691155b50911a6d4e64f392103d39a03
Zach Cohn <zcohn@wustl.edu>
2023-05-30 23:22:02 -0400

    added consignee selector in consignment table

--- src/components/records/Consign/Consign.js
+++ src/components/records/Consign/Consign.js
@@ -6,240 +6,88 @@ import { DatePicker, MobileDatePicker } from "@mui/x-date-pickers";
 import { TextField, Autocomplete, TextareaAutosize } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import ConsigneeInput from "../ConsigneeInput/ConsigneeInput";
 import Dayjs from "dayjs";
 
 function Consign(props) {
-  const [consignees, setConsignees] = useState(["test", "test2"]);
-  const [selectedConsigneeFrom, setSelectedConsigneeFrom] = useState([]);
-  const [selectedConsigneeTo, setSelectedConsigneeTo] = useState([]);
+  const [consignees, setConsignees] = useState([]);
+  const [selectedConsigneeFrom, setSelectedConsigneeFrom] = useState(null);
+  const [selectedConsigneeTo, setSelectedConsigneeTo] = useState(null);
   const [date, setDate] = useState(Dayjs());
 
 
-
-
-
-  async function getConsignees() {
-    const response = await fetch("/api/records/consignment/consignees");
-    const data = await response.json();
-    if (data.success) {
-
-      let consignees = [];
-      data.consignees.forEach((consignee) => {
-        consignees.push({
-          id: consignee.dealerid,
-          name: consignee.dealer,
-          initials: consignee.initials,
-        });
-
-
-      });
-
-
-
-      setConsignees(consignees);
+  function handleFromChange(value) {
+    if (value === null) {
+      setSelectedConsigneeFrom(null);
+      return;
     }
-
+    setSelectedConsigneeFrom(value);
   }
 
-  useEffect(() => {
-    getConsignees();
-  }, []);
-
-  function handleFromChange(event, values) {
-    let selected = {};
-    const selectedValues = Array.isArray(values) ? values : [values];
-    
-    selectedValues.forEach((value) => {
-      if (value) {
-        selected[value.dealerid] = value;
-      }
-    });
-    setSelectedConsigneeFrom(selected);
+  function handleToChange(value) {
+    if (value === null) {
+      setSelectedConsigneeTo(null);
+      return;
+    }
+    setSelectedConsigneeTo(value);
   }
-  function handleToChange(event, values) {
-    let selected = {};
-    const selectedValues = Array.isArray(values) ? values : [values];
-    selectedValues.forEach((value) => {
-      if (value) {
-        selected[value.dealerid] = value;
-      }
-    });
-    setSelectedConsigneeTo(selected);
-  }
-  
 
   function showAllConsignFields() {
     const container = document.querySelector(`.${styles.container}`);
     container.classList.add(styles["show-all-fields"]);
     // after 200ms, show the other fields
     setTimeout(() => {
-        const bottom = document.querySelector(`.${styles.bottom}`);
-        bottom.classList.add(styles["visible"]);
-    }
-    , 275);
+      const bottom = document.querySelector(`.${styles.bottom}`);
+      bottom.classList.add(styles["visible"]);
+    }, 275);
   }
 
-    function hideAllConsignFields() {
-      // hide the other fields
-      const bottom = document.querySelector(`.${styles.bottom}`);
-      bottom.classList.remove(styles["visible"]);
-      // after 200ms, hide the other fields
-      setTimeout(() => {
-        const container = document.querySelector(`.${styles.container}`);
-        container.classList.remove(styles["show-all-fields"]);
-      }, 200);
+  function hideAllConsignFields() {
+    // hide the other fields
+    const bottom = document.querySelector(`.${styles.bottom}`);
+    bottom.classList.remove(styles["visible"]);
+    // after 200ms, hide the other fields
+    setTimeout(() => {
+      const container = document.querySelector(`.${styles.container}`);
+      container.classList.remove(styles["show-all-fields"]);
+    }, 200);
+  }
 
-        
-
+  useEffect(() => {
+    // if both consignees are selected, show all fields
+    if (
+      selectedConsigneeFrom &&
+      selectedConsigneeTo
+    ) {
+      showAllConsignFields();
+    } else {
+      hideAllConsignFields();
     }
+  }, [selectedConsigneeFrom, selectedConsigneeTo]);
 
-    useEffect(() => {
-      // if both consignees are selected, show all fields
-      if (
-        Object.keys(selectedConsigneeFrom).length > 0 &&
-        Object.keys(selectedConsigneeTo).length > 0
-      ) {
-        showAllConsignFields();
-      } else {
-        hideAllConsignFields();
-      }
-    }, [selectedConsigneeFrom, selectedConsigneeTo]);
-    
-
-
-    function evenSpacingInitials(initials) {
-      if (initials.length < 3) {
-        const whitespace = " ".repeat(3 - initials.length); // Calculate the number of whitespace characters needed
-        return `${initials}${whitespace}`;
-      }
-      return initials.slice(0, 3); // Take the first 3 characters of the initials
-    }
-
-
-    useEffect(() => {
-
-      const fromAutoComplete = document.getElementsByClassName(styles["consignee-from-autocomplete"])[0];
-      const toAutoComplete = document.getElementsByClassName(styles["consignee-to-autocomplete"])[0];
-
-
-      // for each list item in the autocomplete, add a focus state for arrow navigation
-      const fromListItems = fromAutoComplete.querySelectorAll("li");
-      fromListItems.forEach((item) => {
-        item.addEventListener('keydown', function(event) {
-          if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-            console.log("arrow up or down");
-            // Remove the selected class from all items
-            dropdownItems.forEach(function(item) {
-              item.classList.remove('focus');
-            });
-      
-            // Add the selected class to the focused item
-            item.classList.add('focus');
-          }
-        });
-      });
-
-      const toListItems = toAutoComplete.querySelectorAll("li");
-      toListItems.forEach((item) => {
-        item.addEventListener('keydown', function(event) {
-          if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-            // Remove the selected class from all items
-            dropdownItems.forEach(function(item) {
-              item.classList.remove('focus');
-            });
-      
-            // Add the selected class to the focused item
-            item.classList.add('focus');
-          }
-        });
-      });
-
-
-      setTimeout(() => {
-        console.log("Frozen");
-      }, 2000);
-
-    }, []);
-    
 
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-      <Autocomplete
-      className={styles["consignee-from-autocomplete"]}
-  sx={{
-    width: 300,
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "black",
-      },
-      "&:hover fieldset": {
-        borderColor: "black",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "black",
-      },
-    },
-  }}
-    renderOption={(props, option) => (
-    <li {...props} className={styles['autocomplete-option']}>
-        <span>{option.name}</span>
-        <span className={styles.initials}>{option.initials}</span>
-    </li>
-  )}
-  options={consignees}
-  getOptionLabel={(option) => option.name}
+        <ConsigneeInput
+          className={styles["consignee-from-autocomplete"]}
+          onChange={handleFromChange}
+        />
 
-  onChange={handleToChange}
-  filterSelectedOptions
-  renderInput={(params) => <TextField {...params} placeholder="From" />}
-  size="small"
-  placeholder="From"
-/>
+        <ArrowRight
+          size={22}
+          style={{ margin: "auto 10px", cursor: "default" }}
+        />
 
-<ArrowRight
-  size={22}
-  style={{ margin: "auto 10px", cursor: "default" }}
-/>
-
-<Autocomplete
-  sx={{
-    width: 300,
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "black",
-      },
-      "&:hover fieldset": {
-        borderColor: "black",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "black",
-      },
-    },
-  }}
-  className={styles["consignee-to-autocomplete"]}
-  renderOption={(props, option) => (
-    <li {...props} className={styles['autocomplete-option']}>
-        <span>{option.name}</span>
-        <span className={styles.initials}>{option.initials}</span>
-    </li>
-  )}
-  options={consignees}
-  getOptionLabel={(option) => option.name}
-
-  onChange={handleFromChange}
-  filterSelectedOptions
-  renderInput={(params) => <TextField {...params} placeholder="To" />}
-  size="small"
-  placeholder="To"
-/>
-
-
+        <ConsigneeInput
+          className={styles["consignee-to-autocomplete"]}
+          onChange={handleToChange}
+        />
       </div>
       <div className={styles.bottom}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
-          className={styles.datepicker}
+            className={styles.datepicker}
             value={date}
             onChange={(newValue) => {
               setDate(newValue);
@@ -248,19 +96,18 @@ function Consign(props) {
               <TextField
                 {...params}
                 sx={{
-                //   width: 300,
+                  //   width: 300,
                   "& .MuiOutlinedInput-root": {
                     "& fieldset": {
-                        borderColor: "black",
+                      borderColor: "black",
                     },
                     "&:hover fieldset": {
-                        borderColor: "black",
+                      borderColor: "black",
                     },
                     "&.Mui-focused fieldset": {
-                        borderColor: "black",
+                      borderColor: "black",
                     },
-                    },
-
+                  },
                 }}
                 size="small"
               />
@@ -269,27 +116,23 @@ function Consign(props) {
         </LocalizationProvider>
 
         <TextField
-            sx={{
-                width: "100%",
-                "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                        borderColor: "black",
-                    },
-                    "&:hover fieldset": {
-                        borderColor: "black",
-                    },
-                    "&.Mui-focused fieldset": {
-                        borderColor: "black",
-                    },
-                },
-
-            }}
-            size="small"
-            placeholder="Notes"
-            
+          sx={{
+            width: "100%",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "black",
+              },
+              "&:hover fieldset": {
+                borderColor: "black",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "black",
+              },
+            },
+          }}
+          size="small"
+          placeholder="Notes"
         />
-
-
 
         <button className={`theme-design ${styles["consign-btn"]}`}>Add</button>
       </div>
---
+++ src/components/records/ConsigneeInput/ConsigneeInput.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+
+import styles from "./ConsigneeInput.module.css";
+
+import { TextField, Autocomplete, TextareaAutosize } from "@mui/material";
+
+function ConsigneeInput(props) {
+  const [consignees, setConsignees] = useState(null);
+  const [selected, setSelected] = useState(null);
+
+
+  // when used in another component, the parent component can pass in "onChange" as a prop
+  // this function will be called when the consignee is changed
+  function handleChange(event, value) {
+    console.log(value);
+    setSelected(value);
+    if (props.onChange) {
+      props.onChange(value);
+    }
+  }
+
+  useEffect(() => {
+    async function getConsignees() {
+        const response = await fetch("/api/records/consignment/consignees");
+        const data = await response.json();
+        if (data.success) {
+    
+          let consignees = [];
+          data.consignees.forEach((consignee) => {
+            consignees.push({
+              id: consignee.dealerid,
+              name: consignee.dealer,
+              initials: consignee.initials,
+            });
+    
+    
+          });
+
+          // now, sort alphabetically
+            consignees.sort((a, b) => {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (b.name < a.name) {
+                    return 1;
+                }
+                return 0;
+            });
+            
+    
+    
+    
+          setConsignees(consignees);
+        }
+        }
+        getConsignees();
+    }, []);
+
+
+
+  return (
+    <Autocomplete
+      className={props.className}
+      sx={{
+        width: 300,
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: "black",
+          },
+          "&:hover fieldset": {
+            borderColor: "black",
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: "black",
+          },
+        },
+      }}
+      renderOption={(props, option) => (
+        <li {...props} className={styles["autocomplete-option"]}>
+          <span>{option.name}</span>
+          <span className={styles.initials}>{option.initials}</span>
+        </li>
+      )}
+      options={consignees}
+      getOptionLabel={(option) => option.name}
+      onChange={handleChange}
+      filterSelectedOptions
+      renderInput={(params) => <TextField {...params} placeholder="From" />}
+      size="small"
+      placeholder="From"
+    />
+  );
+}
+
+export default ConsigneeInput;
