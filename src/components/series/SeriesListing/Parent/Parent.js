5d03d0d9b7334af69073b621ad0f008cc225cda7
Zach Cohn <zcohn@wustl.edu>
2023-06-05 20:53:59 -0400

    fixed aesthetic things

--- src/components/series/SeriesListing/Parent/Parent.js
+++ src/components/series/SeriesListing/Parent/Parent.js
@@ -7,14 +7,26 @@ import LazyLoad from "react-lazyload";
 function Parent(props) {
   const parentWork = props.parentWork;
   const seriesID = props.seriesID;
+  const [marginTop, setMarginTop] = useState("");
+
+  // on render, get the height (offsetHeight) of element with class parent-header
+
+  const parentSpecificClass = "parent-header-" + parentWork.parentID;
+  useEffect(() => {
+    const parentHeader = document.getElementsByClassName(parentSpecificClass)[0];
+    const parentHeaderHeight = parentHeader.offsetHeight;
+    const marginTopString = "calc(-" + parentHeaderHeight + "px - 1rem)";
+    setMarginTop(marginTopString);
+  }, []);
+
 
   return (
     <>
-      <div className={styles.parent + " d-flex flex-row "}>
+      <div className={styles.parent + " d-flex flex-row "} >
         <div className={styles["parent-content"]}>
-          <div className={styles["parent-header"] + " d-flex flex-column justify-content-start"}>
+          <div className={styles["parent-header"] + " d-flex flex-column justify-content-start " + parentSpecificClass}>
             <p className={styles["parent-detail"]}>
-              {parentWork.title}
+              {parentWork.title.toUpperCase()}
             </p>
             <p className={styles["parent-detail"]}>{parentWork.year}</p>
             <div className={styles["parent-medium"]}>
@@ -25,10 +37,13 @@ function Parent(props) {
             </div>
           </div>
 
-          <div className={styles["parent-children"]}>
+          <div className={styles["parent-children"]}
+            style={{ marginTop: marginTop }}
+          >
             {parentWork.children && parentWork.children.length > 0 && parentWork.children.map((child) => (
               <LazyLoad key={child.childID}style={{ width: "100%" }}>
-                <Child childWork={child} seriesID={seriesID} key={child.childID} hideSoldOut={props.hideSoldOut} />
+                <Child childWork={child} seriesID={seriesID} key={child.childID} hideSoldOut={props.hideSoldOut}
+                />
               </LazyLoad>
             ))}
           </div>
